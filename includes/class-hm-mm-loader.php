4c488a560d78b5a5da911af7471fd1dd8ee87861
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2026-01-09 23:53:13 +0300

    style: add mega menu panel layout and hover behavior

--- includes/class-hm-mm-loader.php
+++ includes/class-hm-mm-loader.php
@@ -18,9 +18,32 @@ final class HM_MM_Loader {
   private function __construct() {}
 
   private function init() {
+
     if ( is_admin() ) {
       require_once HM_MM_PATH . 'includes/class-hm-mm-admin-menu-fields.php';
       HM_MM_Admin_Menu_Fields::init();
     }
+
+    // FRONTEND
+    require_once HM_MM_PATH . 'includes/class-hm-mm-walker.php';
+    add_filter('wp_nav_menu_args', [$this, 'inject_walker'], 10, 1);
+    add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
+  }
+
+  public function inject_walker($args) {
+    // Sadece primary menüde çalışsın istersen burada şart ekleriz
+    if ( empty($args['walker']) ) {
+      $args['walker'] = new HM_MM_Walker();
+    }
+    return $args;
+  }
+
+  public function enqueue_assets() {
+    wp_enqueue_style(
+      'hm-mega-menu-front',
+      HM_MM_URL . 'assets/front.css',
+      [],
+      HM_MM_VERSION
+    );
   }
 }
